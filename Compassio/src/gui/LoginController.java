518d461f1e8ff2d2332e5e40d6458ff5aaa8351b
Mazipanen <43139438+Mazipanen@users.noreply.github.com>
2019-03-26 17:53:19 +0100

    Udkast til gui login

--- Compassio/src/gui/LoginController.java
+++ Compassio/src/gui/LoginController.java
@@ -7,7 +7,13 @@ package gui;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,12 +22,47 @@ import javafx.fxml.Initializable;
  */
 public class LoginController implements Initializable {
 
+    @FXML
+    private AnchorPane user_menu;
+    @FXML
+    private ImageView menu_click;
+    @FXML
+    private ImageView menu_close;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        user_menu.setVisible(false);
+        menu_click.setVisible(true);
+        menu_close.setVisible(false);
     }    
+
+    @FXML
+    private void user_menu_slide(MouseEvent event) {
+        user_menu.setVisible(true);
+        menu_click.setVisible(false);
+        menu_close.setVisible(true);
+    }
+
+    @FXML
+    private void user_menu_close(MouseEvent event) {
+        user_menu.setVisible(false);
+        menu_click.setVisible(true);
+        menu_close.setVisible(false);
+    }
+
+    @FXML
+    private void minimise(MouseEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setIconified(true);
+    }
+
+    @FXML
+    private void close(MouseEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.close();
+    }
     
 }
