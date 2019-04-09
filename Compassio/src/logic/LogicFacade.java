d6f3e4dfda0a6fe86ce248de2c3bfe6676a15631
Peterzxcvbnm <43370082+Peterzxcvbnm@users.noreply.github.com>
2019-04-09 14:54:34 +0200

    added socialworker case

--- Compassio/src/logic/LogicFacade.java
+++ Compassio/src/logic/LogicFacade.java
@@ -30,16 +30,19 @@ public class LogicFacade implements ILogic {
     @Override
     public ArrayList<Case> getCases() {
         ArrayList<Case> response = new ArrayList<>();
-        if (user.isInstanceOf(SocialWorker)) {
+        if (user.isInstanceOf(CaseWorker)) {
             ArrayList<String[]> cases = persistence.getCasesByUserID(user.getID());
             String[] singleCase = cases.remove(cases.size() - 1);
             response.add(new Case(singleCase[0], singleCase[1], Integer.parseInt(singleCase[2]), Integer.parseInt(singleCase[3]),
                     singleCase[4], singleCase[5], singleCase[6], singleCase[7], Integer.parseInt(singleCase[8])), singleCase[9]);
-        }
-    
-        
+        } else if (user.isInstanceOf(SocialWorker)) {
+            ArrayList<String[]> cases = persistence.getCasesByDepartment(user.getDepartmentID());
+            String[] singleCase = cases.remove(cases.size() - 1);
+            response.add(new Case(singleCase[0], singleCase[1], Integer.parseInt(singleCase[2]), Integer.parseInt(singleCase[3]),
+                    singleCase[4], singleCase[5], singleCase[6], singleCase[7], Integer.parseInt(singleCase[8])), singleCase[9]);
 
-    
+        }
+    }
 
     public void createCase(long CPR, int[] socialWorkers) {
         UUID caseID = UUID.randomUUID();
