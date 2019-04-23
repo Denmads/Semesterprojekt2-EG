fb58c32ccd71c466e4e4e758f77a4e294e6cfcef
Mads Jensen <madsj18@student.sdu.dk>
2019-04-23 12:46:27 +0200

    Added password hashing

--- Compassio/src/compassio/Compassio.java
+++ Compassio/src/compassio/Compassio.java
@@ -9,9 +9,12 @@ import acquaintance.IGUI;
 import acquaintance.ILogic;
 import acquaintance.IPersistence;
 import gui.GUIrun;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import logic.LogicFacade;
 import persistence.PersistenceFacade;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -31,7 +34,8 @@ public class Compassio extends Application {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        
         IGUI gui = new GUIrun();
         ILogic logic = new LogicFacade();
         IPersistence persistence = new PersistenceFacade();
--- Compassio/src/persistence/PersistenceFacade.java
+++ Compassio/src/persistence/PersistenceFacade.java
@@ -7,10 +7,16 @@ package persistence;
 
 import java.sql.*;
 import acquaintance.IPersistence;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
 import java.util.ArrayList;
 import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
 
 /**
  *
@@ -105,23 +111,81 @@ public class PersistenceFacade implements IPersistence {
     public String[] getUser(String username, String password) {
         String[] user = new String[4];
         try (Connection db = DriverManager.getConnection(dbIP, this.username, this.password);
-                PreparedStatement statement = db.prepareStatement("SELECT * FROM people WHERE username=? AND hashedpassword =?")) {
+                PreparedStatement statement = db.prepareStatement("SELECT salt FROM people WHERE username=?")) {
             statement.setString(1, username);
-            statement.setString(2, password);
             ResultSet rs = statement.executeQuery();
 
             if (rs.next() == false) {
                 return null;
             } else {
-                user[0] = rs.getString("userid");
-                user[1] = rs.getString("username");
-                user[2] = rs.getString("firstname");
-                user[3] = rs.getString("lastname");
-                return user;
+                byte[] salt = rs.getBytes("salt");
+                
+                try (PreparedStatement checkStatement = db.prepareStatement("SELECT * FROM people WHERE username=? AND hashedpassword=?")) {
+                    checkStatement.setString(1, username);
+                    checkStatement.setBytes(2, hashPassword(password, salt));
+                    ResultSet res = checkStatement.executeQuery();
+                    
+                    if (res.next()) {
+                        user[0] = res.getString("userid");
+                        user[1] = res.getString("username");
+                        user[2] = res.getString("firstname");
+                        user[3] = res.getString("lastname");
+                        return user;
+                    }
+                    else {
+                        return null;
+                    }
+                } catch (NoSuchAlgorithmException | InvalidKeySpecException ex) {
+                    ex.printStackTrace();
+                    return null;
+                }
+                
             }
         } catch (SQLException ex) {
             ex.printStackTrace();
             return null;
+        }
+    }
+    
+    private byte[] generateSalt () {
+        SecureRandom secRan = new SecureRandom();
+        byte[] salt = new byte[16];
+        secRan.nextBytes(salt);
+        return salt;
+    }
+    
+    private byte [] hashPassword (String password, byte[] salt) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 128);
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+
+        return factory.generateSecret(spec).getEncoded();
+    }
+    
+    public static void main (String[] args) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        //Used for creating users
+        
+    }
+    
+    //Used fro creating users
+    public void createUser () throws NoSuchAlgorithmException, InvalidKeySpecException {
+        byte[] salt = generateSalt();
+        byte[] pass = hashPassword("admin", salt);
+        
+        try (Connection db = DriverManager.getConnection(dbIP, username, password);
+                PreparedStatement statement = db.prepareStatement("INSERT INTO people VALUES (?, ?, ?, ?, ?, ?)")) {
+            statement.setLong(1, 1L);
+            statement.setString(2, "admin");
+            statement.setString(3, "admin");
+            statement.setString(4, "admin");
+            statement.setBytes(5, pass);
+            statement.setBytes(6, salt);
+            
+             System.out.println(statement);
+            
+            statement.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println("SQL exception");
+            ex.printStackTrace();
         }
     }
 
