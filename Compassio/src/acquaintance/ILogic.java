1afcf264d5a2454677555a7c43c15f2e05263380
Morten Kargo <5476181+Kargo@users.noreply.github.com>
2019-04-23 14:28:37 +0200

    Menu name/role update. (#12)
    * Menu name/role update.
    
    After login in the users name and role is now displayed in the hamburger menu
    
    * Removed licence header template
    
    Added a function to get department names by ID
    Removed licence header template from multiple files

--- Compassio/src/acquaintance/ILogic.java
+++ Compassio/src/acquaintance/ILogic.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package acquaintance;
 
 import java.util.ArrayList;
@@ -21,5 +16,8 @@ public interface ILogic {
     public void createCase(long CPR, int[] socialWorkers);
 
     public ArrayList<String> retrieveCaseTypes ();
-     public boolean login(String username, String password);
+    public boolean login(String username, String password);
+    public String getUserName();
+    public String getUserType();
+    public String getDepartmentNameById(int departmentId);
 }
