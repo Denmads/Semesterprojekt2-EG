af6389092fb130d9eab313753a3a46247e29c80e
Morten Kargo Lyngesen <mortenkargo@gmail.com>
2019-05-23 13:00:16 +0200

    Finished CprDAO

--- Compassio/src/persistence/dataaccessobjects/CprDAO.java
+++ Compassio/src/persistence/dataaccessobjects/CprDAO.java
@@ -3,37 +3,81 @@ package persistence.dataaccessobjects;
 import persistence.util.ArgumentParser;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.apache.commons.dbcp2.BasicDataSource;
 
 /**
  * Provides standard operations for the cpr table in the database.
+ *
  * @author kargo
  */
 public class CprDAO implements DataAccessObject {
+
     private final BasicDataSource connectionPool;
 
-    public CprDAO () {
+    public CprDAO() {
         this.connectionPool = DatabaseConnection.getInstance().getConnectionPool();
     }
 
     @Override
     public String[] get(String... id) {
-      return null;  
+        if (id.length < 1) {
+            return null;
+        }
+        try (Connection db = connectionPool.getConnection();
+                PreparedStatement statement = db.prepareStatement("SELECT * FROM cpr WHERE cprnumber=?")) {
+            statement.setInt(1, (int) Integer.parseInt(id[0]));
+            ResultSet rs = statement.executeQuery();
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int columnLength = rsmd.getColumnCount();
+            if (rs.next() == false) {
+                return null;
+            } else {
+                String[] person = new String[columnLength - 1];
+                for (int i = 1; i < columnLength; i++) {
+                    person[i - 1] = rs.getString(i);
+                }
+                return person;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(CaseDAO.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
     }
 
     @Override
     public List<String[]> getAll(String... cond) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ArrayList<String[]> people = new ArrayList<>();
+        try (Connection db = connectionPool.getConnection();
+                PreparedStatement statement = db.prepareStatement("SELECT * FROM cpr")) {
+            ResultSet rs = statement.executeQuery();
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int columnLength = rsmd.getColumnCount();
+            while (rs.next()) {
+                String[] person = new String[columnLength - 1];
+                for (int i = 1; i < columnLength; i++) {
+                    person[i - 1] = rs.getString(i);
+                }
+                people.add(person);
+            }   
+                return people;    
+        } catch (SQLException ex) {
+            return null;
+        }
     }
 
     @Override
     public boolean create(String... args) {
         Map<String, List<String>> options = ArgumentParser.parse(args);
         if (!options.containsKey("cpr") && !options.containsKey("firstname")
-                && !options.containsKey("lastname")) { 
+                && !options.containsKey("lastname")) {
             return false;
         }
         try (Connection db = connectionPool.getConnection();
@@ -50,9 +94,12 @@ public class CprDAO implements DataAccessObject {
 
     @Override
     public boolean update(long id, String[] args) {
-         try (Connection db = connectionPool.getConnection();
+        Map<String, List<String>> options = ArgumentParser.parse(args);
+        try (Connection db = connectionPool.getConnection();
                 PreparedStatement statement = db.prepareStatement("UPDATE cpr SET firstname = ?, lasttname= ? WHERE cprnumber=?")) {
-            statement.setLong(1, id);
+            statement.setString(1, options.get("firstname").get(0));
+            statement.setString(1, options.get("lastname").get(0));
+            statement.setLong(3, id);
             statement.executeQuery();
             return true;
         } catch (SQLException ex) {
