f6b0e02e97d0524235ef3542becc2aae2441e3d1
Morten Kargo Lyngesen <mortenkargo@gmail.com>
2019-05-25 22:09:34 +0200

    Removed unused imports

--- Compassio/src/compassio/Compassio.java
+++ Compassio/src/compassio/Compassio.java
@@ -4,15 +4,9 @@ import acquaintance.IGUI;
 import acquaintance.ILogic;
 import acquaintance.IPersistence;
 import gui.GUIrun;
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
 import logic.LogicFacade;
 import persistence.PersistenceFacade;
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
